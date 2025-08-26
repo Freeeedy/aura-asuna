d54365f922a8edfe2290c60b55e6e688507f3363
Freeeedy <hadraba87@gmail.com>
2025-08-26 07:08:04 +0200

    Aug 26th 2025
    transferred to pc and continued in the course ended at 4:50:20 and at 7:07 AM

---
+++ Data_extractor.py
@@ -0,0 +1,54 @@
+import os
+import lzma
+from tqdm import tqdm
+import concurrent.futures
+import random
+
+def xz_files_in_dir(directory):
+    files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".xz") and os.path.isfile(os.path.join(directory, filename)):
+            files.append(filename)
+    return files
+
+folder_path = "C:/Users/Uživatel/Downloads/urlsf_subset01/openwebtext"
+output_file_train = "output_train.txt"
+output_file_val = "output_val.txt"
+vocab_file = "vocab.txt"
+
+
+files = xz_files_in_dir(folder_path)
+total_files = len(files)
+
+# calculate the split indices
+split_index = int(total_files * 0.9) # 90% for training
+files_train = files[:split_index]
+files_val = files[split_index:]
+
+# process the files for training and validation separetely
+vocab = set()
+
+# process the training files
+with open(output_file_train, "w", encoding="utf-8") as outfile:
+    for filename in tqdm(files_train, total=len(files_train)):
+        file_path = os.path.join(folder_path, filename)
+        with lzma.open(file_path, "rt", encoding="utf-8", errors="ignore") as infile:
+            text = infile.read()
+            outfile.write(text)
+            characters = set(text)
+            vocab.update(set(text))
+
+# process the validation files
+with open(output_file_val, "w", encoding="utf-8") as outfile:
+    for filename in tqdm(files_val, total=len(files_val)):
+        file_path = os.path.join(folder_path, filename)
+        with lzma.open(file_path, "rt", encoding="utf-8", errors="ignore") as infile:
+            text = infile.read()
+            outfile.write(text)
+            characters = set(text)
+            vocab.update(set(text))
+
+# write the vocab to vocab.txt
+with open(vocab_file, "w", encoding="utf-8") as vfile:
+    for char in vocab:
+        vfile.write(char + '\n')
